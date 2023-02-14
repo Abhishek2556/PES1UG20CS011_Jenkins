edfb0f7936f6bfafd1a16d01e8306b80197a5b44
Abhishek2556 <74259654+Abhishek2556@users.noreply.github.com>
2023-02-14 15:00:27 +0530

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  cout << "Hello, I am Abhishek Singhi SRN: PES1UG20CS011\n";
+  cout << "Welcome to India\n";
+  return 0;
+}
